ba4dc8149f94f1e4821263d4b168e9ae5ee6471a
Juan David Peña <juandavidp76@hotmail.com>
2023-11-29 18:55:32 -0500

    perf menu dropdown

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -6,15 +6,20 @@ import { useState } from 'react';
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+
   const menuClass =
     'self-center text-lg font-bold text-white hover:text-gray-600 cursor-pointer duration-500';
 
+  const dropdownClass = isOpen ? 'block' : 'hidden';
+
   return (
     <nav className='flex w-screen bg-teal-500 justify-between px-20 h-16 fixed'>
-      <div className={menuClass} onClick={toggle}>
-        <div>Historia</div>
-        <div className={isOpen ? 'block' : 'hidden'}>
-          <ul>
+      <div className={`relative ${menuClass}`}>
+        <div onClick={toggle}>
+          Historia
+        </div>
+        <div className={`absolute top-12 left-0 ${dropdownClass}`}>
+          <ul className='bg-teal-500 p-4 shadow-xl rounded-md'>
             <li className={menuClass}>Cocteleria</li>
             <li className={menuClass}>Destilados</li>
             <li className={menuClass}>Cocteles</li>
@@ -24,7 +29,6 @@ export default function Header() {
       <Link className={menuClass} href={'/cocktailcard'}>
         <div>Recetas</div>
       </Link>
-
       <Link className={menuClass} href={'/cocktailsform'}>
         <div>Nuevo Cocktail</div>
       </Link>
