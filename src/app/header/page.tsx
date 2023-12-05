b8ac36b1927794bd69f632e6989276f2056c21cc
Juan David Peña <juandavidp76@hotmail.com>
2023-12-05 14:56:03 -0500

    perf header

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -8,32 +8,89 @@ export default function Header() {
   const toggle = () => setIsOpen(!isOpen);
 
   const menuClass =
-    'self-center text-lg font-bold text-white hover:text-gray-600 cursor-pointer duration-500';
+    'sm:self-center text-lg font-bold text-white hover:text-gray-600 cursor-pointer duration-500';
 
   const dropdownClass = isOpen ? 'block' : 'hidden';
 
   return (
-    <nav className='flex w-screen bg-teal-500 justify-between px-20 h-16 fixed'>
-      <div className={`relative ${menuClass}`}>
-        <div onClick={toggle}>
-          Historia
-        </div>
-        <div className={`absolute top-12 left-0 ${dropdownClass}`}>
-          <ul className='bg-teal-500 p-4 shadow-xl rounded-md'>
-            <li className={menuClass}>Cocteleria</li>
-            <li className={menuClass}>Destilados</li>
-            <li className={menuClass}>Cocteles</li>
-          </ul>
-        </div>
+    <nav className='sm:flex w-screen bg-teal-500 px-4 sm:px-20 sm:h-16 fixed'>
+      {/* Hamburger Icon for Mobile */}
+      <div className='flex items-center sm:hidden duration-500 h-16'>
+        <button className='text-white' onClick={toggle}>
+          <svg
+            className='w-8 h-7'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+          >
+            <path
+              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+            />
+          </svg>
+        </button>
       </div>
-      <Link className={menuClass} href={'/cocktailcard'}>
-        <div>Recetas</div>
-      </Link>
-      <Link className={menuClass} href={'/cocktailsform'}>
-        <div>Nuevo Cocktail</div>
-      </Link>
-      <div className={menuClass}>Todo sobre cocteleria</div>
-      <div className={menuClass}>Contacto</div>
+
+      {/* Logo */}
+      <div className='hidden sm:block relative'>
+        {/* Your logo or brand */}
+        {/* For example: <img src='/logo.png' alt='Logo' className='h-8' /> */}
+      </div>
+
+      {/* Menu items */}
+      <div
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } sm:flex sm:w-auto sm:space-x-20 -ml-2 -mt-5 sm:ml-0 sm:mt-0 bg-teal-500 h-40 sm:h-auto sm:px-0 px-4 sm:py-0 py-4`}
+        id='menu'
+      >
+        <div className={`relative ${menuClass}`}>
+          <div onClick={toggle}>Historia</div>
+          {/* <div className={`absolute top-12 left-0 ${dropdownClass}`}>
+            <ul className='bg-teal-500 p-4 shadow-xl rounded-md'>
+              <li className={menuClass}>Cocteleria</li>
+              <li className={menuClass}>Destilados</li>
+              <li className={menuClass}>Cocteles</li>
+            </ul>
+          </div> */}
+        </div>
+        <Link className={menuClass} href={'/cocktailcard'}>
+          <div>Recetas</div>
+        </Link>
+        <Link className={menuClass} href={'/cocktailsform'}>
+          <div>Nuevo Cocktail</div>
+        </Link>
+        <div className={menuClass}>Todo sobre cocteleria</div>
+        <div className={menuClass}>Contacto</div>
+      </div>
     </nav>
   );
 }
+
+//   return (
+//     <nav className='flex flex-wrap md:flex-nowrap w-screen bg-teal-500 justify-between px-20 h-16 fixed'>
+//       <div className={`relative ${menuClass} md:mb-0 mb-4`}>
+//         <div onClick={toggle}>
+//           Historia
+//         </div>
+//         <div className={`absolute top-12 left-0 ${dropdownClass}`}>
+//           <ul className='bg-teal-500 p-4 shadow-xl rounded-md'>
+//             <li className={menuClass}>Cocteleria</li>
+//             <li className={menuClass}>Destilados</li>
+//             <li className={menuClass}>Cocteles</li>
+//           </ul>
+//         </div>
+//       </div>
+//       <Link className={menuClass} href={'/cocktailcard'}>
+//         <div>Recetas</div>
+//       </Link>
+//       <Link className={menuClass} href={'/cocktailsform'}>
+//         <div>Nuevo Cocktail</div>
+//       </Link>
+//       <div className={menuClass}>Todo sobre cocteleria</div>
+//       <div className={menuClass}>Contacto</div>
+//     </nav>
+//   );
+// }
