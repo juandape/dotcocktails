b18cc57e8ca98b693b10af850c921537db5e909a
Juan David Peña <juandavidp76@hotmail.com>
2023-12-26 16:55:23 -0500

    fix rights

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className='bg-gradient-to-t from-black-top to-blue-tp fixed bottom-0 w-screen h-10'>
       <div>
         <p className='text-center text-white sm:text-sm text-xs mt-4'>
-          &copy; {new Date().getFullYear()} Dot Cocktails. Todos los derechos reservados.
+          &copy; {new Date().getFullYear()} Dot Cocktails. All rights reserved.
           <a
             className='text-blue-tp'
           >
