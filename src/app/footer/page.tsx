10f9a9d6bd8cbb38ec5cf016b31a31f85b241d52
Juan David Peña <juandavidp76@hotmail.com>
2024-02-12 20:28:56 -0500

    feat: logo to root

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -1,18 +1,15 @@
 export default function Footer() {
-  const currentTimestamp = Date.now();
-  const currentDate = new Date(currentTimestamp);
-  const currentYear = currentDate.getUTCFullYear();
   return (
     <footer className='bg-gradient-to-t from-black-top to-blue-tp fixed bottom-0 w-screen h-10 cursor-default'>
       <div>
         <p className='text-center text-white sm:text-sm text-xs mt-4'>
-          Copyright &copy; {currentYear}{' '}
+          Copyright &copy; 2019-2024
           <a
             className='hover:text-peach-fuzz duration-500'
             href='https://juandape.dev'
             target='blank'
           >
-            <em> juandape.dev </em>
+            <strong> juandape.dev - Dot Cocktails </strong>
           </a>
           All rights reserved.
           <a className='text-blue-tp'></a>
--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from 'react';
 import { GoPerson, GoSearch } from 'react-icons/go';
 
 import Modal from '@/components/login-modal';
+import { menuClass, subMenuClass } from '@/components/styles';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -103,286 +104,292 @@ export default function Header() {
     }
   };
 
-  const menuClass =
-    'xl:self-center mb-2 2xl:text-lg text-lg font-bold text-peach-fuzz hover:text-cofee-1 cursor-pointer duration-500';
-  const subMenuClass =
-    'xl:bg-gradient-to-b from-black-top to-blue-tp w-60 xl:w-60 xl:shadow-xl px-4 pb-7 xl:pb-4 xl:pt-4 rounded-md';
-
   const dropdownClass = isOpen ? 'block' : 'hidden';
 
   return (
-    <nav className='xl:flex w-screen xl:justify-center bg-gradient-to-b from-black-top to-blue-tp px-4 xl:px-20 xl:h-16'>
-      {/* Hamburger Icon for Mobile */}
-      <div
-        className='flex items-center xl:hidden duration-500 h-16'
-        onClick={toggle}
-        ref={menuRef}
-      >
-        <button className='text-white'>
-          <svg
-            className='w-8 h-7'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-          >
-            <path
-              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            />
-          </svg>
-        </button>
-      </div>
-
-      {/* Menu items */}
-      <div
-        className={`${
-          isOpen ? 'block' : 'hidden'
-        } xl:flex xl:w-auto xl:space-x-20 -ml-2 xl:ml-0 xl:mt-0 xl:bg-gradient-to-b from-black-top to-blue-tp h-50 xl:h-auto xl:px-0 px-4 xl:py-0 py-4`}
-        id='menu'
-        ref={menuRef}
-      >
-        <div className={`mr-10 ${menuClass}`}>
-          <Link href='/search'>
-            <GoSearch className='text-xl' />
-          </Link>
-        </div>
-        <div className={`xl:relative -mt-2 sm:mt-0 ${menuClass}`}>
-          <div onClick={() => handleDropdown('historia')}>Historia ▿</div>
-          {selected === 'historia' && (
-            <div
-              className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
-            >
-              <ul className={subMenuClass}>
-                <Link
-                  className={menuClass}
-                  href={'/history/cocktail'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Cocktail</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/beer'}
-                  onClick={toggle}
-                >
-                  <li>Historia de la Cerveza</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/gin'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Gin</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/rum'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Ron</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/tequila'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Tequila</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/wine'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Vino</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/vodka'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Vodka</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/history/whisky'}
-                  onClick={toggle}
-                >
-                  <li>Historia del Whisky</li>
-                </Link>
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className={`xl:relative -mt-2 sm:mt-0 ${menuClass}`}>
-          <div onClick={() => handleDropdown('recetas')}>Recetas ▿</div>
-          {selected === 'recetas' && (
-            <div
-              className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
-            >
-              <ul className={subMenuClass}>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/beer'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Cerveza</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/gin'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Gin</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/rum'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Ron</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/tequila'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Tequila</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/wine'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Vino</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/vodka'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Vodka</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/whisky'}
-                  onClick={toggle}
-                >
-                  <li>Cocteles con Whisky</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/other'}
-                  onClick={toggle}
-                >
-                  <li>Otros Cocteles</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/mocktails'}
-                  onClick={toggle}
-                >
-                  <li>Mocktails</li>
-                </Link>
-                <Link
-                  className={menuClass}
-                  href={'/recipes/syrups'}
-                  onClick={toggle}
-                >
-                  <li>Jarabes</li>
-                </Link>
-              </ul>
-            </div>
-          )}
-        </div>
-        <Link
-          className={menuClass}
-          href={'/under-construction'}
+    <div className='flex bg-gradient-to-b from-black-top to-blue-tp'>
+      <nav className='xl:flex justify-between w-screen xl:justify-center px-4 xl:px-20 xl:h-16'>
+        {/* Hamburger Icon for Mobile */}
+        <div
+          className='flex items-center xl:hidden duration-500 h-16'
           onClick={toggle}
+          ref={menuRef}
         >
-          <div className='-mt-2 sm:mt-0'>Todo sobre cocteleria</div>
-        </Link>
-        <Link
-          className={menuClass}
-          href={'/under-construction'}
-          onClick={toggle}
-        >
-          <div>Servicios</div>
-        </Link>
-        <Link className={menuClass} href={'/about'} onClick={toggle}>
-          <div>Acerca de</div>
-        </Link>
-        <Link className={menuClass} href={'/contact'} onClick={toggle}>
-          <div>Contacto</div>
-        </Link>
-        {userRole === 'ADMIN' ? (
-          <div className={`xl:relative ${menuClass}`}>
-            <div onClick={() => handleDropdown('nuevo')}>Admin Tools ▿</div>
-            {selected === 'nuevo' && (
-              <div
-                className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
-              >
-                <ul className={subMenuClass}>
-                  <Link
-                    className={menuClass}
-                    href={'/create/cocktails-form'}
-                    onClick={toggle}
-                  >
-                    <li>Nuevo Cocktail</li>
-                  </Link>
-                  <Link
-                    className={menuClass}
-                    href={'/create/history-form'}
-                    onClick={toggle}
-                  >
-                    <li>Nueva Historia</li>
-                  </Link>
-                </ul>
-              </div>
-            )}
-          </div>
-        ) : null}
-        {userRole ? (
-          <div className={`xl:relative ${menuClass}`}>
-            <div onClick={() => handleDropdown('user')}>
-              <Image
-                alt='Avatar'
-                className='rounded-full'
-                height={40}
-                src={userAvatar || ''}
-                width={40}
+          <button className='text-white'>
+            <svg
+              className='w-8 h-7'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+            >
+              <path
+                d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
               />
-            </div>
-            {selected === 'user' && (
+            </svg>
+          </button>
+        </div>
+
+        {/* Menu items */}
+        <div
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } xl:flex xl:w-auto xl:space-x-20 -ml-2 xl:ml-0 xl:mt-0 xl:bg-gradient-to-b from-black-top to-blue-tp h-50 xl:h-auto xl:px-0 px-4 xl:py-0 py-4`}
+          id='menu'
+          ref={menuRef}
+        >
+          <div className={`mr-10 ${menuClass}`}>
+            <Link href='/search' onClick={toggle}>
+              <GoSearch className='text-xl' />
+            </Link>
+          </div>
+          <div className={`xl:relative -mt-2 sm:mt-0 ${menuClass}`}>
+            <div onClick={() => handleDropdown('historia')}>Historia ▿</div>
+            {selected === 'historia' && (
               <div
                 className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
               >
                 <ul className={subMenuClass}>
                   <Link
                     className={menuClass}
-                    href={`/create/users-form?id=${id}`}
+                    href={'/history/cocktail'}
                     onClick={toggle}
                   >
-                    <li>Perfil</li>
+                    <li>Historia del Cocktail</li>
                   </Link>
-                  <div className={menuClass} onClick={toggle}>
-                    <li onClick={handleClose}>Logout</li>
-                  </div>
+                  <Link
+                    className={menuClass}
+                    href={'/history/beer'}
+                    onClick={toggle}
+                  >
+                    <li>Historia de la Cerveza</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/gin'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Gin</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/rum'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Ron</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/tequila'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Tequila</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/wine'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Vino</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/vodka'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Vodka</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/history/whisky'}
+                    onClick={toggle}
+                  >
+                    <li>Historia del Whisky</li>
+                  </Link>
                 </ul>
               </div>
             )}
           </div>
-        ) : (
-          <button
-            className={`font-extrabold border-2 border-peach-fuzz rounded-full hover:border-cofee-1 ${menuClass}`}
-            onClick={openModal}
+          <div className={`xl:relative -mt-2 sm:mt-0 ${menuClass}`}>
+            <div onClick={() => handleDropdown('recetas')}>Recetas ▿</div>
+            {selected === 'recetas' && (
+              <div
+                className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
+              >
+                <ul className={subMenuClass}>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/beer'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Cerveza</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/gin'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Gin</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/rum'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Ron</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/tequila'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Tequila</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/wine'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Vino</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/vodka'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Vodka</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/whisky'}
+                    onClick={toggle}
+                  >
+                    <li>Cocteles con Whisky</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/other'}
+                    onClick={toggle}
+                  >
+                    <li>Otros Cocteles</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/mocktails'}
+                    onClick={toggle}
+                  >
+                    <li>Mocktails</li>
+                  </Link>
+                  <Link
+                    className={menuClass}
+                    href={'/recipes/syrups'}
+                    onClick={toggle}
+                  >
+                    <li>Jarabes</li>
+                  </Link>
+                </ul>
+              </div>
+            )}
+          </div>
+          <Link
+            className={menuClass}
+            href={'/under-construction'}
+            onClick={toggle}
           >
-            <GoPerson className='text-xl' />
-          </button>
-        )}
-        {isModalOpen && <Modal isOpen={false} onClose={closeModal} />}
-      </div>
-    </nav>
+            <div className='-mt-2 sm:mt-0'>Todo sobre cocteleria</div>
+          </Link>
+          <Link
+            className={menuClass}
+            href={'/under-construction'}
+            onClick={toggle}
+          >
+            <div>Servicios</div>
+          </Link>
+          <Link className={menuClass} href={'/about'} onClick={toggle}>
+            <div>Acerca de</div>
+          </Link>
+          <Link className={menuClass} href={'/contact'} onClick={toggle}>
+            <div>Contacto</div>
+          </Link>
+          {userRole === 'ADMIN' ? (
+            <div className={`xl:relative ${menuClass}`}>
+              <div onClick={() => handleDropdown('nuevo')}>Admin Tools ▿</div>
+              {selected === 'nuevo' && (
+                <div
+                  className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
+                >
+                  <ul className={subMenuClass}>
+                    <Link
+                      className={menuClass}
+                      href={'/create/cocktails-form'}
+                      onClick={toggle}
+                    >
+                      <li>Nuevo Cocktail</li>
+                    </Link>
+                    <Link
+                      className={menuClass}
+                      href={'/create/history-form'}
+                      onClick={toggle}
+                    >
+                      <li>Nueva Historia</li>
+                    </Link>
+                  </ul>
+                </div>
+              )}
+            </div>
+          ) : null}
+          {userRole ? (
+            <div className={`xl:relative ${menuClass}`}>
+              <div onClick={() => handleDropdown('user')}>
+                <Image
+                  alt='Avatar'
+                  className='rounded-full'
+                  height={40}
+                  src={userAvatar || ''}
+                  width={40}
+                />
+              </div>
+              {selected === 'user' && (
+                <div
+                  className={`xl:absolute top-7 xl:top-10 left-0 ${dropdownClass}`}
+                >
+                  <ul className={subMenuClass}>
+                    <Link
+                      className={menuClass}
+                      href={`/create/users-form?id=${id}`}
+                      onClick={toggle}
+                    >
+                      <li>Perfil</li>
+                    </Link>
+                    <div className={menuClass} onClick={toggle}>
+                      <li onClick={handleClose}>Logout</li>
+                    </div>
+                  </ul>
+                </div>
+              )}
+            </div>
+          ) : (
+            <button
+              className={`font-extrabold border-2 border-peach-fuzz rounded-full hover:border-cofee-1 ${menuClass}`}
+              onClick={openModal}
+            >
+              <GoPerson className='text-xl' />
+            </button>
+          )}
+          {isModalOpen && <Modal isOpen={false} onClose={closeModal} />}
+        </div>
+      </nav>
+      <Link href='/'>
+        <Image
+          alt='Logo'
+          className='cursor-pointer mt-2 mr-10 sm:mr-20'
+          height={50}
+          src='https://res.cloudinary.com/dpvmwsbq8/image/upload/v1706739163/upload-folder/logoBlanco_k0nyhu.png'
+          width={50}
+        />
+      </Link>
+    </div>
   );
 }
